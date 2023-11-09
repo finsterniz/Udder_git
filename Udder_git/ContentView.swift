5f2b93e54a40390412a0023cf78fb0e706132063
boiltaimn <34036969+finsterniz@users.noreply.github.com>
2023-11-09 09:55:26 +0100

    add a cow

--- Udder_git/ContentView.swift
+++ Udder_git/ContentView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image("cow")
+                .resizable()
+                .frame(width: 200,height: 200)
+                .aspectRatio(contentMode: .fit)
+                
+            Text("Hello, cow world!")
         }
         .padding()
     }
