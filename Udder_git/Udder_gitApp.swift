2efe1664e00018bd58d487c71d012ce906f6144d
boiltaimn <34036969+finsterniz@users.noreply.github.com>
2023-11-09 09:44:10 +0100

    Initial Commit

---
+++ Udder_git/Udder_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Udder_gitApp.swift
+//  Udder_git
+//
+//  Created by a on 09.11.23.
+//
+
+import SwiftUI
+
+@main
+struct Udder_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
